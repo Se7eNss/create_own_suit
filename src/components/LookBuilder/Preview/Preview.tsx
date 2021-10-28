1d3793a35966a1993819b3bb6aee943c7cf64cc1
Se7eNss <oguzhanseven18@gmail.com>
2021-10-28 15:21:48 +0300

    v2

--- src/components/LookBuilder/Preview/Preview.tsx
+++ src/components/LookBuilder/Preview/Preview.tsx
@@ -4,6 +4,8 @@ const Preview = () => {
     return (
         <div className="col-6" >
             <img src="/asset/default-img/preview-default.png" alt=""  />
+            <div></div>
+            <div></div>
         </div>
     )
 }
