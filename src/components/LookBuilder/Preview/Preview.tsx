a6a9c842d2126da4b2a4fcd50e55fe1c21a822e6
Oğuzhan Seven <52214388+Se7eNss@users.noreply.github.com>
2021-11-05 22:24:37 +0300

    Update Preview.tsx

--- src/components/LookBuilder/Preview/Preview.tsx
+++ src/components/LookBuilder/Preview/Preview.tsx
@@ -28,13 +28,6 @@ const Preview = () =>  {
                 <VestAndCummerbund/>
                 <Belt/>
             </div>
-
-        <div className="col-6" >
-            <img src="https://gentux.imgix.net/look-builder/gentux/v80/130074BLK-jacket_front.png" />
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
     )
 }
 
